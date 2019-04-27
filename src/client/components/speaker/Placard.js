c9a84b58e03ce2477ddfffeb510e8827219459b5
Evelyn Hathaway <git@evelyn.dev>
2019-04-27 04:01:05 -0700

    Fix typo

--- src/client/components/speaker/Placard.js
+++ src/client/components/speaker/Placard.js
@@ -19,7 +19,7 @@ export default class Placard extends React.Component {
             <div className="card text-center my-3 placard" onClick={this.lowerPlacard.bind(this)}>
                 <div className="card-body">
                     <h5>{countryName}</h5>
-                    <span>Raised, tap to lower</span>>
+                    <span>Raised, tap to lower</span>
                 </div>
             </div>
         );
