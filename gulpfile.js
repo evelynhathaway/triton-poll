d1b8855a375fd438422708d036524ba0e9bd0dad
Griffen Schwiesow <gs@schwigri.com>
2019-10-26 19:25:11 -0700

    Update: Add gulp for CSS prefixing

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const groupmq = require('gulp-group-css-media-queries');
+
+const STYLE_SOURCES = [
+    './public/**/*.css',
+];
+
+function prefixStyles() {
+    return gulp.src(STYLE_SOURCES, { base: './' })
+        .pipe(plumber())
+        .pipe(postcss([
+            autoprefixer({
+                cascade: false
+            })
+        ]))
+        .pipe(groupmq())
+        .pipe(gulp.dest('.'));
+}
+exports.prefixStyles = prefixStyles;
+
+exports.default = gulp.series(prefixStyles);
